fae6f6862cbe755584fa8478b8675a68c3796ac9
Isaias-dt <Isaias-dt@hotmail.com>
2019-08-20 23:47:41 -0300

    Adicionado os metodos enqueue, isEmpty, isFull, size. Obs: metodo dequeue não esta completo e retorna null.

--- src/fila/model/Fila.java
+++ src/fila/model/Fila.java
@@ -10,6 +10,40 @@ package fila.model;
  * @author isaia
  */
 public class Fila {
-   
+    private Object[] elementos;
+    private int tamanho, capFila;
+    private Object prim, ult;
     
+    public Fila(int capacidade){
+        elementos = new Object[capacidade];
+        this.tamanho = 0;
+        capFila = capacidade;
+        
+    }
+    
+    public void enqueue(Object elemento) throws Exception{
+        if(this.tamanho == 0){
+            this.elementos[tamanho] = elemento;
+            tamanho++;
+        }else{
+            this.elementos[tamanho] = elemento;
+        }   tamanho++;
+    }
+    
+    public Object dequeue(){
+        
+        return null;
+    }
+    
+    public int size(){
+        return this.tamanho;
+    }
+    
+    public boolean isEmpty(){
+        return this.tamanho == 0;
+    }
+    
+    public boolean isFull(){
+        return this.tamanho == capFila;        
+    }
 }
